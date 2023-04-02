3ee5ceff637586865bdd7d1d919f1294203ee149
zqzq83 <56616597+zqzq83@users.noreply.github.com>
2023-04-02 16:05:01 +0800

    Update x.php

--- api/x.php
+++ api/x.php
@@ -13,7 +13,6 @@
         $current.= $stream.rtrim(chunk_split($timestamp, 3, "/"), "/").".ts"."\r\n";
         $timestamp = $timestamp + 1;
     }
-    header("Content-Type: audio/x-mpegurl");
-    header("Content-Disposition: attachment; filename=playlist.m3u8");
+    header("Content-Type: text/plain");
     echo $current;
 ?>
