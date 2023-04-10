093289cc46caa432f4ab556c5e55e298b250af4a
zqzq83 <56616597+zqzq83@users.noreply.github.com>
2023-04-10 15:03:35 +0800

    Update a.php

--- api/a.php
+++ api/a.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set("Asia/Shanghai");
-$channel = empty($_GET['id']) ? "emdy4k_8000" : trim($_GET['id']);
+$channel = empty($_GET['id']) ? "CCTV-xw_4000" : trim($_GET['id']);
 $array = explode("_", $channel);
 if (isset($array[1])) {
     $stream = "http://[2409:8c62:7110:10c::12]/live2.rxip.sc96655.com/live/8ne5i_sccn,{$array[0]}_hls_pull_{$array[1]}K/";
