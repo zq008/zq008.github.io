a500b37cb26d381cab5268b3e26cfd68ed013e8e
zqzq83 <56616597+zqzq83@users.noreply.github.com>
2023-04-26 15:33:36 +0800

    Update a.php

--- api/a.php
+++ api/a.php
@@ -1,13 +1,13 @@
 <?php
     $channel = empty($_GET['id']) ? "CCTV-xw_4000" : trim($_GET['id']);
     $array = explode("_", $channel);
-    $stream = "http://[2409:8c02:21c:60::28]/live2.rxip.sc96655.com/live/8ne5i_sccn/{$array[0]}_hls_pull/8ne5i_sccn,{$array[0]}_hls_pull_{$array[1]}K/";
+    $stream = "http://[2408:873d:2010:1000::4]/live2.rxip.sc96655.com/live/8ne5i_sccn/{$array[0]}_hls_pull/8ne5i_sccn,{$array[0]}_hls_pull_{$array[1]}K/";
     $timestamp = intval((time()-60)/6);
     $current = "#EXTM3U"."\r\n";
     $current.= "#EXT-X-VERSION:3"."\r\n";
     $current.= "#EXT-X-TARGETDURATION:6"."\r\n";
     $current.= "#EXT-X-MEDIA-SEQUENCE:{$timestamp}"."\r\n";
-    for ($i=0; $i<3; $i++)
+    for ($i=0; $i<6; $i++)
     {
         $current.= "#EXTINF:6,"."\r\n";
         $current.= $stream.rtrim(chunk_split($timestamp, 3, "/"), "/").".ts"."\r\n";
